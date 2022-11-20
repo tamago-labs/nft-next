cbfceb41ec64a4d893e999c5694c3f1805efd5d3
pisuthd <pisuth.dae@gmail.com>
2022-11-20 14:41:03 +0900

    fix: non-evm error

--- FEVM-Hardhat-Kit/contracts/utils/Soulbound1155.sol
+++ FEVM-Hardhat-Kit/contracts/utils/Soulbound1155.sol
@@ -525,25 +525,25 @@ contract Soulbound1155 is Context, ERC165, IERC1155, IERC1155MetadataURI {
         uint256 amount,
         bytes memory data
     ) private {
-        if (to.isContract()) {
-            try
-                IERC1155Receiver(to).onERC1155Received(
-                    operator,
-                    from,
-                    id,
-                    amount,
-                    data
-                )
-            returns (bytes4 response) {
-                if (response != IERC1155Receiver.onERC1155Received.selector) {
-                    revert("ERC1155: ERC1155Receiver rejected tokens");
-                }
-            } catch Error(string memory reason) {
-                revert(reason);
-            } catch {
-                revert("ERC1155: transfer to non-ERC1155Receiver implementer");
-            }
-        }
+        // if (to.isContract()) {
+        //     try
+        //         IERC1155Receiver(to).onERC1155Received(
+        //             operator,
+        //             from,
+        //             id,
+        //             amount,
+        //             data
+        //         )
+        //     returns (bytes4 response) {
+        //         if (response != IERC1155Receiver.onERC1155Received.selector) {
+        //             revert("ERC1155: ERC1155Receiver rejected tokens");
+        //         }
+        //     } catch Error(string memory reason) {
+        //         revert(reason);
+        //     } catch {
+        //         revert("ERC1155: transfer to non-ERC1155Receiver implementer");
+        //     }
+        // }
     }
 
     function _doSafeBatchTransferAcceptanceCheck(
@@ -554,27 +554,27 @@ contract Soulbound1155 is Context, ERC165, IERC1155, IERC1155MetadataURI {
         uint256[] memory amounts,
         bytes memory data
     ) private {
-        if (to.isContract()) {
-            try
-                IERC1155Receiver(to).onERC1155BatchReceived(
-                    operator,
-                    from,
-                    ids,
-                    amounts,
-                    data
-                )
-            returns (bytes4 response) {
-                if (
-                    response != IERC1155Receiver.onERC1155BatchReceived.selector
-                ) {
-                    revert("ERC1155: ERC1155Receiver rejected tokens");
-                }
-            } catch Error(string memory reason) {
-                revert(reason);
-            } catch {
-                revert("ERC1155: transfer to non-ERC1155Receiver implementer");
-            }
-        }
+        // if (to.isContract()) {
+        //     try
+        //         IERC1155Receiver(to).onERC1155BatchReceived(
+        //             operator,
+        //             from,
+        //             ids,
+        //             amounts,
+        //             data
+        //         )
+        //     returns (bytes4 response) {
+        //         if (
+        //             response != IERC1155Receiver.onERC1155BatchReceived.selector
+        //         ) {
+        //             revert("ERC1155: ERC1155Receiver rejected tokens");
+        //         }
+        //     } catch Error(string memory reason) {
+        //         revert(reason);
+        //     } catch {
+        //         revert("ERC1155: transfer to non-ERC1155Receiver implementer");
+        //     }
+        // }
     }
 
     function _asSingletonArray(uint256 element)
